c5f66f4dc96b9619a57ebf8d64fbc70e241ef3f3
zachary kadish <zachkadish@gmail.com>
2017-12-15 10:02:20 -0700

    api is now server a list of coins

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(function(req, res, next) {
 });
 
 app.use('/', index);
-app.use('/api/:stock', api);
+app.use('/api', api);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,38 @@
-var express = require('express');
-var router = express.Router();
-// var data = require('../data/appl.json');
+const https = require('https');
+const express = require('express');
+const router = express.Router();
 
-/*GET stock data. */
-router.get('/', function(req, res, next) {
-  // res.send(data);
-})
+const coinListUrl = 'https://www.cryptocompare.com/api/data/coinlist/';
+let coinData = '';
+let coins = '';
+
+/**
+ * GET list of coins...
+ */
+https.get(coinListUrl, res => {
+  res.setEncoding('utf8');
+  res.on('data', data => {
+    coinData += data;
+  });
+  res.on('end', () => {
+    coinData = JSON.parse(coinData);
+    coins = Object.values(coinData.Data).reduce((a, cur, i, arr) => {
+      const sortNum = i + 1;
+      const coin = arr.filter((c) => {
+        return Number(c.SortOrder) === sortNum;
+      })[0];
+      a.push(coin);
+      return a;
+    }, [])
+    // console.log(coins);
+  });
+});
+
+/**
+ * api list of first 100 coins form cryptocompare
+ */
+router.get('/coins', function(req, res, next) {
+  res.send(coins.slice(0, 100));
+});
 
 module.exports = router;
